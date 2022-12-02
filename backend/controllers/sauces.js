4214e0462392a4688a8fc375e882f734d7074789
Angie pons <a.lartillier@gmail.com>
2022-12-02 16:43:01 +0100

    Fonction like mise a jour

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -39,16 +39,17 @@ exports.getOneSauce = (req, res, next) => {
 
 
 
-
+// Fonction pour modifier un objet de la requête Post /api/sauces/:id
 exports.modifySauce = (req, res, next) => {
 
-    // Si j'ai une image je modifie la propieté imageUrl pour lui donner le chemin, sinon je recupère le body
+    // Si j'ai une image dans ma requête alors je la traite et je modifie la propriété imageUrl pour lui donner le chemin, sinon je recupère le body
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : {
         ...req.body
     }
+
 
     delete sauceObject._userId
 
@@ -107,34 +108,7 @@ exports.likeSauce = (req, res, next) => {
             Sauce.findOne({ _id: sauceId })
                 .then((sauce) => {
 
-                    // si l'utilisateur est dans le tableau des usersLiked, retire un like a la sauce et supprime l'utilisateur du tableau des likes de la sauce ($pull)
-                    if (sauce.usersLiked.find(user => user === userId)) {
-                        Sauce.updateOne({ _id: sauceId }, { $inc: { likes: -1 }, $pull: { usersLiked: userId }, _id: sauceId })
-
-                            .then(() => res.status(200).json({ message: "Avis supprimé !" }))
-                            .catch((error) => res.status(400).json({ error }))
-                    }
-                    // si l'utilisateur se trouve dans le tableau des userDisliked, retire un dislike du tableau des dislikes
-                    if (sauce.usersDisliked.find(user => user === userId)) {
-                        Sauce.updateOne({ _id: sauceId },
-                            {
-                                $inc: { dislikes: -1 },
-                                $pull: { usersDisliked: userId },
-                                _id: sauceId
-                            })
-
-                            .then(() => res.status(200).json({ message: "Avis supprimé !" }))
-                            .catch((error) => res.status(400).json({ error }))
-                    }
-                })
-                .catch((error) => res.status(400).json({ error }))
-            break
-
-        // Si le like passe a 1, recupere la sauce de ma DB et met a jour le nombre de like sur la sauce, verifie si l'utilsateur n'a pas deja like
-        case 1:
-
-            Sauce.findOne({ _id: sauceId })
-                .then((sauce) => {
+                    // si l'user est déjà dans le tableau des usersLiked, retire un like au compteur des likes et supprime l'user du tableau des likes ($pull)
                     if (sauce.usersLiked.find(user => user === userId)) {
                         Sauce.updateOne({ _id: sauceId },
                             {
@@ -145,25 +119,8 @@ exports.likeSauce = (req, res, next) => {
 
                             .then(() => res.status(200).json({ message: "Avis supprimé !" }))
                             .catch((error) => res.status(400).json({ error }))
-                    } else {
-                        Sauce.updateOne({ _id: sauceId },
-                            {
-                                $inc: { likes: 1 },
-                                $push: { usersLiked: userId },
-                                _id: sauceId
-                            })
-                            .then(() => res.status(200).json({ message: "Avis donné !" }))
-                            .catch((error) => res.status(400).json({ error }))
                     }
-                })
-                .catch((error) => res.status(400).json({ error }))
-
-            break
-        // Si l'utilisateur clic sur l'avis negatif, alors on ajoute un dislike 
-        // si l'utilisateur avait deja donné un avis negatif, retire la valeur du dislike  et supprime l'utilisateur du tableau des disliked 
-        case -1:
-            Sauce.findOne({ _id: sauceId })
-                .then((sauce) => {
+                    // si l'user se trouve dans le tableau des userDisliked, retire un dislike du compteur des dislikes et retire l'user du tableau des dislikes
                     if (sauce.usersDisliked.find(user => user === userId)) {
                         Sauce.updateOne({ _id: sauceId },
                             {
@@ -172,20 +129,38 @@ exports.likeSauce = (req, res, next) => {
                                 _id: sauceId
                             })
 
-                            .then(() => res.status(200).json({ message: "Avis negatif supprimé !" }))
-                            .catch((error) => res.status(400).json({ error }))
-                    } else {
-                        Sauce.updateOne({ _id: sauceId },
-                            {
-                                $inc: { dislikes: 1 },
-                                $push: { usersDisliked: userId },
-                                _id: sauceId
-                            })
-                            .then(() => res.status(200).json({ message: "Avis negatif donné !" }))
+                            .then(() => res.status(200).json({ message: "Avis supprimé !" }))
                             .catch((error) => res.status(400).json({ error }))
                     }
                 })
                 .catch((error) => res.status(400).json({ error }))
+            break
+
+        // Si le like passe a 1, met a jour le nombre de like et ajoute l'id de l'user dans le tableau des usersLiked
+        case 1:
+            Sauce.updateOne({ _id: sauceId },
+                {
+                    $inc: { likes: 1 },
+                    $push: { usersLiked: userId },
+                    _id: sauceId
+                })
+                .then(() => res.status(200).json({ message: "Avis donné !" }))
+                .catch((error) => res.status(400).json({ error }))
+
+            break
+
+        // Si l'user met un dislike, alors on ajoute un dislike au compteur des dislikes de la sauce et on stock l'id de l'user dans le tableau des usersDisliked
+        case -1:
+
+            Sauce.updateOne({ _id: sauceId },
+                {
+                    $inc: { dislikes: 1 },
+                    $push: { usersDisliked: userId },
+                    _id: sauceId
+                })
+                .then(() => res.status(200).json({ message: "Avis negatif donné !" }))
+                .catch((error) => res.status(400).json({ error }))
+
             break
         default:
             console.log(`il n y a pas de ${like}`)
